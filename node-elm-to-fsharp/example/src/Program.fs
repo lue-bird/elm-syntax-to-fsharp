bf29d8b5667fcea7958a2b2f053ab33b7b063bb2
lue-bird <lue.the.bird@gmail.com>
2024-12-28 11:17:27 +0100

    script and example

---
+++ node-elm-to-fsharp/example/src/Program.fs
@@ -0,0 +1,9 @@
+﻿module Program
+
+let tst: {| Name: string |} -> string = _.Name
+
+[<EntryPoint>]
+let main args =
+    let input = stdin.ReadToEnd()
+    stdout.Write(Elm.formatSingleModule_formatSingleModule input)
+    0
