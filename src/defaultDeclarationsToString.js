0054dca020e2a3729d4e9981ce040fd3d608c2ae
lue-bird <lue.the.bird@gmail.com>
2025-06-29 23:57:09 +0200

    add script to make updating ElmSyntaxToFsharp.defaultDeclarations easier

---
+++ src/defaultDeclarationsToString.js
@@ -0,0 +1,26 @@
+// turn DefaultDeclarations.fs into the escaped elm string
+// to be inserted at ElmSyntaxToFsharp.defaultDeclarations
+
+import * as fs from "node:fs"
+import * as path from "node:path"
+
+
+const defaultDeclarationsFsharpFile =
+    fs.readFileSync(
+        path.join(import.meta.dirname, "DefaultDeclarations.fs"),
+        { encoding: "utf-8" }
+    )
+const elmString =
+    "\"\"\""
+    + defaultDeclarationsFsharpFile
+        .replaceAll(
+            `namespace global
+
+module Elm =`,
+            ""
+        ).replaceAll("\\", "\\\\")
+    + "\"\"\""
+fs.writeFileSync(
+    path.join(import.meta.dirname, "DefaultDeclarations.elm"),
+    elmString, { encoding: "utf-8" }
+)
