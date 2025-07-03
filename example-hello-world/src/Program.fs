bb0589d65014603893bdcbe76fa95fa4a87b6d4a
lue-bird <lue.the.bird@gmail.com>
2025-07-03 22:51:39 +0200

    example hello-world simplify F# a little bit

--- example-hello-world/src/Program.fs
+++ example-hello-world/src/Program.fs
@@ -3,8 +3,8 @@
 [<EntryPoint>]
 let main (_arguments: array<string>) : int =
     let greeting: string =
-        Elm.StringRope.toString
-            (Elm.Hello_greet (Elm.StringRopeOne "world"))
-    System.Console.WriteLine(greeting)
+        Elm.StringRope.toString (Elm.Hello_greet(Elm.StringRopeOne "world"))
+
+    System.Console.WriteLine greeting
 
     0
